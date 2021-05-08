28069d95e5ada053e78ce49741e908bf58a82a4c
Devin Hopkins <devinhopkins@gmail.com>
2021-05-08 17:10:56 -0400

    added umls api

--- main.py
+++ main.py
@@ -3,12 +3,49 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
+import requests
+from bs4 import BeautifulSoup
+import os
+import json
+import pandas as pd
+
 from scispacy.abbreviation import AbbreviationDetector
 from scispacy.linking import EntityLinker
 
-nlp = spacy.load("en_core_sci_sm", disable=["tok2vec", "tagger", "attribute_ruler", "lemmatizer", "parser"])
+nlp = spacy.load("en_core_sci_sm", disable=[
+                 "tok2vec", "tagger", "attribute_ruler", "lemmatizer", "parser"])
 
 app = FastAPI()
+
+
+def get_tgt():
+    url = 'https://utslogin.nlm.nih.gov/cas/v1/api-key'
+    myobj = {'apikey': os.environ.get('umls_api_key')}
+    response = requests.post(url, data=myobj)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tgt = soup.find('form').get('action')
+    return tgt
+
+
+def get_st(tgt):
+    url = tgt
+    service = 'http://umlsks.nlm.nih.gov'
+    myobj = {'service': service}
+
+    response = requests.post(url, data=myobj)
+    st = response.text
+    return st
+
+
+def atom_to_cui(search_term, tgt):
+    st = get_st(tgt)
+    x = requests.get(
+        f'https://uts-ws.nlm.nih.gov/rest/search/current?string={search_term}&ticket={st}')
+    results = json.loads(x.text)['result']['results']
+    return results[0]['ui']
+
+
+tgt = get_tgt()
 
 
 class UserRequestIn(BaseModel):
@@ -20,6 +57,7 @@ class EntityOut(BaseModel):
     end: int
     type: str
     text: str
+    cui: str
 
 
 class EntitiesOut(BaseModel):
@@ -37,6 +75,6 @@ def read_entities(user_request_in: UserRequestIn):
                 "end": ent.end_char,
                 "type": ent.label_,
                 "text": ent.text,
-            } for ent in doc.ents
+                "cui": atom_to_cui(ent.text, tgt)} for ent in doc.ents
         ]
     }
